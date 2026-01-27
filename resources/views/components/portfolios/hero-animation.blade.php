61a2d832eb388a41701c0c55bc2da557c36dceff
Mathieu <mathieu-moreau@hotmail.com>
2026-01-27 15:44:11 -0500

    Changement dans le hero

--- resources/views/components/portfolios/hero-animation.blade.php
+++ resources/views/components/portfolios/hero-animation.blade.php
@@ -1,19 +1,21 @@
-<section id="hero" x-data="heroAnimation"
+ <section id="hero" x-data="heroAnimation"
      class="flex flex-col items-center justify-center relative overflow-hidden h-screen font-hero font-thin text-blanc">
-     <div class="relative z-10 text-center w-full px-4">
+
+     <div class="relative z-10 text-center w-full">
          <div x-show="showName" x-transition:enter="transition ease-out duration-1000"
              x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
              x-transition:leave="transition ease-in duration-1000" x-transition:leave-start="opacity-100"
              x-transition:leave-end="opacity-0" class="absolute inset-0 flex flex-col items-center justify-center">
-             <h1 class="text-5xl sm:text-7xl md:text-8xl lg:text-[6rem]">
+             <h1 class="text-[3.25rem] sm:text-[6rem] flex flex-col">
                  <span>Mathieu Moreau</span>
              </h1>
          </div>
+
          <div x-show="!showName" x-cloak x-transition:enter="transition ease-out duration-1000"
              x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
              x-transition:leave="transition ease-in duration-1000" x-transition:leave-start="opacity-100"
-             x-transition:leave-end="opacity-0" class="absolute inset-0 flex flex-col items-center justify-center px-4">
-             <p class="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-[5rem] uppercase tracking-wide sm:tracking-widest">
+             x-transition:leave-end="opacity-0" class="absolute inset-0 flex flex-col items-center justify-center">
+             <p class="text-[2.5rem] sm:text-[5rem] uppercase tracking-wide sm:tracking-widest px-4">
                  DÉVELOPPEUR WEB
              </p>
          </div>
