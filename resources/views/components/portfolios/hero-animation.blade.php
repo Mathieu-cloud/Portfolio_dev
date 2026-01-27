f3f1095f85a4acc54de5bb38ece8af9c1340270a
Mathieu <mathieu-moreau@hotmail.com>
2026-01-27 15:53:34 -0500

    Modification de la font size texte

--- resources/views/components/portfolios/hero-animation.blade.php
+++ resources/views/components/portfolios/hero-animation.blade.php
@@ -2,20 +2,20 @@
      class="flex flex-col items-center justify-center relative overflow-hidden h-screen font-hero font-thin text-blanc">
 
      <div class="relative z-10 text-center w-full">
-         <div x-show="showName" x-transition:enter="transition ease-out duration-1000"
+         <div x-show="showName" x-transition:enter="transition ease-out duration-2000 delay-800"
              x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
-             x-transition:leave="transition ease-in duration-1000" x-transition:leave-start="opacity-100"
+             x-transition:leave="transition ease-in duration-2000" x-transition:leave-start="opacity-100"
              x-transition:leave-end="opacity-0" class="absolute inset-0 flex flex-col items-center justify-center">
              <h1 class="text-[3.25rem] sm:text-[6rem] flex flex-col">
                  <span>Mathieu Moreau</span>
              </h1>
          </div>
 
-         <div x-show="!showName" x-cloak x-transition:enter="transition ease-out duration-1000"
+         <div x-show="!showName" x-cloak x-transition:enter="transition ease-out duration-2000 delay-800"
              x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
-             x-transition:leave="transition ease-in duration-1000" x-transition:leave-start="opacity-100"
+             x-transition:leave="transition ease-in duration-2000" x-transition:leave-start="opacity-100"
              x-transition:leave-end="opacity-0" class="absolute inset-0 flex flex-col items-center justify-center">
-             <p class="text-[2.5rem] sm:text-[5rem] uppercase tracking-wide sm:tracking-widest px-4">
+             <p class="text-[2.5rem] sm:text-[5rem] uppercase">
                  DÉVELOPPEUR WEB
              </p>
          </div>
