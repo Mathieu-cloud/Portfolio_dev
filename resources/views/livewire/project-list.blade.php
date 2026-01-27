aecda0b4737bf782244e19e9d6d2e47424603664
Mathieu <mathieu-moreau@hotmail.com>
2026-01-27 15:02:28 -0500

    Amélioration du hero et des liens de projets

--- resources/views/livewire/project-list.blade.php
+++ resources/views/livewire/project-list.blade.php
@@ -1,19 +1,20 @@
 <div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         @forelse ($projets as $projet)
             <div
                 class="bg-gray-800 border border-gray-700 rounded-md overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-300">
                 <div class="h-48 overflow-hidden">
                     {{-- Image --}}
-                    @if ($projet->image)
-                        <img src="{{ asset($projet->image) }}" alt="{{ $projet->nom }}"
-                            class="w-full h-full object-cover">
-                    @else
-                        <div class="w-full h-full bg-gray-700 flex items-center justify-center">
-                            <span class="text-gray-500">Pas d'image</span>
-                        </div>
-                    @endif
+                    <a href="{{ $projet->lien }}" target="_blank">
+                        @if ($projet->image)
+                            <img src="{{ asset($projet->image) }}" alt="{{ $projet->nom }}"
+                                class="w-full h-full object-cover">
+                        @else
+                            <div class="w-full h-full bg-gray-700 flex items-center justify-center">
+                                <span class="text-gray-500">Pas d'image</span>
+                            </div>
+                        @endif
+                    </a>
                 </div>
                 {{-- Nom et description du projet --}}
                 <div class="p-5">
@@ -34,19 +35,7 @@
 
                     {{-- Lien vers site du projet et Edit --}}
                     <div class="flex items-center justify-between mt-auto">
-                        @if ($projet->lien)
-                            <a href="{{ $projet->lien }}" target="_blank"
-                                class="text-indigo-400 hover:text-indigo-300 text-sm font-semibold flex items-center">
-                                Voir le site
-                                <svg class="w-4 h-4 ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14">
-                                    </path>
-                                </svg>
-                            </a>
-                        @endif
-
-                        {{-- CONDITION POUR L'ADMINISTRATION --}}
+                       {{-- CONDITION POUR L'ADMINISTRATION --}}
                         @if ($isAdmin ?? false)
                             <a href="{{ route('admins.projects.edit', $projet) }}"
                                 class="text-gray-400 hover:text-yellow-500 text-sm font-semibold flex items-center transition-colors">
